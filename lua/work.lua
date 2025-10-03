bba0a97e381bf6098ad589087ec730fe9282b47d
Jonah Brupbacher <jonahtylerb@gmail.com>
2025-10-03 16:17:39 -0400

    feat: add copilot-native and sidekick.nvim

--- lua/work.lua
+++ lua/work.lua
@@ -1,25 +1,13 @@
 -- Plugins enabled when I'm working
 if vim.g.working then
   return {
-    {
-      "neovim/nvim-lspconfig",
-      opts = {
-        servers = {
-          eslint = {
-            settings = {
-              workingDirectories = { mode = "auto" },
-              format = false,
-            },
-          },
-        },
-      },
-    },
-    { import = "lazyvim.plugins.extras.ai.copilot-chat" },
-    { import = "lazyvim.plugins.extras.ai.codeium" },
+    { import = "lazyvim.plugins.extras.ai.copilot-native" },
+    { import = "lazyvim.plugins.extras.ai.sidekick" },
+    -- { import = "lazyvim.plugins.extras.ai.codeium" },
   }
 end
 
--- Plugins disabled when I'm working
+-- Plugins enabled when I'm not working
 if not vim.g.working then
   return {
     { import = "lazyvim.plugins.extras.ai.codeium" },
