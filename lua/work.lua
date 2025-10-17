070a124b9f9d292bff4c1bfad9d97996dbf4bb6f
Jonah Brupbacher <jonahtylerb@gmail.com>
2025-10-17 13:56:23 -0400

    fix: switch back to windsurf :(

--- lua/work.lua
+++ lua/work.lua
@@ -1,9 +1,7 @@
 -- Plugins enabled when I'm working
 if vim.g.working then
   return {
-    { import = "lazyvim.plugins.extras.ai.copilot-native" },
-    { import = "lazyvim.plugins.extras.ai.sidekick" },
-    -- { import = "lazyvim.plugins.extras.ai.codeium" },
+    { import = "lazyvim.plugins.extras.ai.codeium" },
   }
 else
   return {
